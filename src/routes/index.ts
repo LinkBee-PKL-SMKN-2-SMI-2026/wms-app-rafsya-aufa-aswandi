6697cbe6486c4e0938fb19605fb34794d934a540
Der-21 <rafsyaaufaaswandi@gmail.com>
2026-08-24 00:32:47 +0700

    feat:Reporting Summary Low-Stock

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import categoryRoute from './category.route';
 import locationRoute from './location.route';
 import productRoute from './product.route';
 import movementRoute from './stock-movement.route';
+import reportRoute from './reporting.route';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/categories', categoryRoute);
 router.use('/locations', locationRoute);
 router.use('/products', productRoute);
 router.use('/movements', movementRoute);
+router.use('/reports', reportRoute);
 
 export default router;
